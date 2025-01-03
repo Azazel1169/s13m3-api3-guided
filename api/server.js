1be9e12f69fbc2610857c0f0c387b4aa88fe422f
Josh Rime <joshrime1990@icloud.com>
2025-01-03 11:50:23 -0600

    testing vscode setting

--- api/server.js
+++ api/server.js
@@ -1,21 +1,26 @@
-const express = require('express'); // importing a CommonJS module
-
-const hubsRouter = require('./hubs/hubs-router.js');
-
-const server = express();
+const express = require("express"); // importing a CommonJS module
+const morgan = require("morgan");
+const hubsRouter = require("./hubs/hubs-router.js");
+            
+function customMorgan(req, res, next) {
+  console.log(`you made a ${req.method} request`);
+  next();
+}
 
 server.use(express.json());
+server.use(morgan("dev"));
+server.use(customMorgan);
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`
     <h2>Hubs API</h2>
     <p>Welcome to the Hubs API</p>
   `);
 });
 
-server.use('*', (req, res) => {
+server.use("*", (req, res) => {
   // catch all 404 errors middleware
   res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
 });
